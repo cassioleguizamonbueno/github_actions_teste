fc502980073d879efd1388df948fd53162849537
cassioleguizamonbueno <36665958+cassioleguizamonbueno@users.noreply.github.com>
2026-06-02 17:32:34 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,3 @@
-/*import pandas as pd
-
-data = {
-  'Nome' : ['Aline', 'Bob', 'Charlie'],
-  'Idade': [25, 30, 35],
-  'Cidade': ['SP', 'RJ', 'BH']
-}
-
-df = pd.DataFrame(data)
-
-print(" ---- DataFrame Pandas --- ")
-print(df)
-print(" ------------------------- ")
-*/
 from fastapi import FastAPI
 
 app = FastAPI()
